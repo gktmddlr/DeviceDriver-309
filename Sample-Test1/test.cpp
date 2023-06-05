78e1d00f18c006e563a3b428f3ca22b48ad28a68
sssikha <gktmddlr@gmail.com>
2023-06-05 15:23:36 +0900

    Read Exception test

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -15,9 +15,24 @@ public:
 TEST(DeviceDriver, ReadTest) {
 	FlashMemoryDeviceMock mock;
 
-	EXPECT_CALL(mock, read).Times(5).WillRepeatedly(Return(0x03));
+	EXPECT_CALL(mock, read(_)).Times(5).WillRepeatedly(Return(0x03));
 
 	DeviceDriver device_driver(&mock);
 
 	EXPECT_THAT(device_driver.read(0x0), Eq(0x3));
+}
+
+TEST(DeviceDriver, ReadFailTest) {
+	FlashMemoryDeviceMock mock;
+
+	EXPECT_CALL(mock, read(_))
+		.WillOnce(Return(0x3))
+		.WillOnce(Return(0x3))
+		.WillOnce(Return(0x3))
+		.WillOnce(Return(0x3))
+		.WillOnce(Return(0x5));
+
+	DeviceDriver device_driver(&mock);
+
+	EXPECT_THROW(device_driver.read(0x0), std::exception);
 }
