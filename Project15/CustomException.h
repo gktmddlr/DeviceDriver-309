ef2e5a71bea2e390f923a593df388b625fbb66f0
sssikha <gktmddlr@gmail.com>
2023-06-05 15:02:49 +0900

    Device Driver Read Behavior logic & test

---
+++ Project15/CustomException.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <exception>
+
+class ReadFailException : public std::exception
+{
+	
+};
--- Project15/DeviceDriver.cpp
+++ Project15/DeviceDriver.cpp
@@ -1,12 +1,29 @@
 #include "DeviceDriver.h"
+#include "CustomException.h"
+#include <windows.h>
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
+void DeviceDriver::readException(unsigned char read_data, unsigned char data)
+{
+	if(read_data != data) {
+		throw ReadFailException();
+	}
+}
+
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    unsigned char read_data = 0;
+    for(int i = 0; i < 5; i++)
+    {
+        auto data = m_hardware->read(address);
+        Sleep(200);
+        if (i != 0)
+            readException(read_data, data);
+        read_data = data;
+    }
+    return (int)read_data;
 }
 
 void DeviceDriver::write(long address, int data)
